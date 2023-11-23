8e62ade178824aa82b7f2f3bc4618b7dbb79923b
Niels van den Dool <nielsvdd42@gmail.com>
2023-11-24 00:30:02 +0100

    first setup and header

---
+++ src/components/Heading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Heading(){
+    return <header>
+    <img className="img-heading"src="/images/logo.png" alt="logo"></img>
+    <h1>InsigneScout</h1>
+    </header>
+}
+
+export default Heading
